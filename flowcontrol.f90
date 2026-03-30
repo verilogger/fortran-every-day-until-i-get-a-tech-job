7b1ea1d8fb91305046c5e65e9568338acabe1d1b
isa <154abello@gmail.com>
2026-03-30 15:18:47 -0700

    YESS..... YES....

--- flowcontrol.f90
+++ flowcontrol.f90
@@ -7,6 +7,14 @@ program flow
     character(:), allocatable :: second_word
 
     allocate(character(16) :: first_word)
-    print *, 
+    print *, 'Enter the first word!!!'
+    read (*,'(A)') first_word
 
-    read()
+    allocate(character(16) :: second_word)
+    print *, 'Enter the second word!!!'
+    read (*,'(A)') second_word
+
+    print *, "The first word was: ", first_word
+    print *, "The second word was: ", second_word
+
+end program flow
