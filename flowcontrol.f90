1d2ee9868ef42082076f1341cb9aeb5d3de0b823
isa <154abello@gmail.com>
2026-03-30 15:44:52 -0700

    it could be winnable... Soon

--- flowcontrol.f90
+++ flowcontrol.f90
@@ -2,19 +2,31 @@
 
 program flow
     implicit none
+
+    character(len=1), allocatable :: first_word(:)
+    character(len=1), allocatable :: second_word(:)
+
+    character(len=100) :: buffer
+
+    integer :: n
+    integer :: i
+
+    n = len_trim(buffer)
+
+    allocate(first_word(n))
     
-    character(:), allocatable :: first_word
-    character(:), allocatable :: second_word
-
-    allocate(character(16) :: first_word)
     print *, 'Enter the first word!!!'
-    read (*,'(A)') first_word
-
-    allocate(character(16) :: second_word)
+    read *, buffer
+    first_word = (/(buffer(i:i), i=1,n)/)
+    
+    allocate(second_word(n))
     print *, 'Enter the second word!!!'
-    read (*,'(A)') second_word
+    read *, buffer
+    second_word = (/(buffer(i:i), i=1,n)/)
 
     print *, "The first word was: ", first_word
     print *, "The second word was: ", second_word
+
+    print *, (first_word(i), i=1, n), " thinks ", (second_word(i), i=1, n), "is PEAK !!!"
 
 end program flow
